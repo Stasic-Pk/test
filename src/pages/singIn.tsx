4f910b4963e4432ae6d68305685b3b36ea6b4985
Stanislav Petryk <stasps210@gmail.com>
2023-07-11 00:08:46 +0300

    bug fix

--- src/pages/singIn.tsx
+++ src/pages/singIn.tsx
@@ -5,12 +5,12 @@ export function SingIn() {
     <>
       <div className="backGround">
         <div className="content">
-          <h1>Sing In</h1>
-          <input className="input" type="text" placeholder="Email" />
-          <input className="input" type="text" placeholder="Password" />
-          <small className="newAccount">Create new account</small>
-          <button className="button">Sign-in</button>
-          <small>or</small>
+          <h1>Sing In</h1> <br/>
+          <input className="input" type="text" placeholder="Email" /> <br/>
+          <input className="input" type="text" placeholder="Password" /> <br/>
+          <small className="newAccount">Create new account</small> <br/>
+          <button className="button">Sign-in</button> <br/>
+          <small>or</small> <br/>
           <button className="button">Continue with Google</button>
         </div>
       </div>
