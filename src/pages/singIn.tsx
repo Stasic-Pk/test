add049ef33e664f798d8aae3f06c77928b7b4678
Stanislav Petryk <stasps210@gmail.com>
2023-07-11 20:43:01 +0300

    update

--- src/pages/singIn.tsx
+++ src/pages/singIn.tsx
@@ -4,14 +4,14 @@ export function SingIn() {
   return (
     <>
       <div className="backGround">
-        <div className="content">
-          <h1>Sing In</h1> <br/>
+        <div>
+          <h1 className="SingIn"> Sing-In </h1> <br/>
           <input className="input" type="text" placeholder="Email" /> <br/>
           <input className="input" type="text" placeholder="Password" /> <br/>
-          <small className="newAccount">Create new account</small> <br/>
-          <button className="button">Sign-in</button> <br/>
-          <small>or</small> <br/>
-          <button className="button">Continue with Google</button>
+          <small className="newAccount"> Create new account </small> <br/>
+          <button className="button"> Sign-in </button> <br/>
+          <small className="or"> or </small> <br/>
+          <button className="button"> Continue with Google </button>
         </div>
       </div>
     </>
