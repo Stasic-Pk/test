c062d3b6319d8b0bf8fffc36ba0eebf8fba1d00c
Stanislav Petryk <stasps210@gmail.com>
2023-08-02 16:38:32 +0300

    update and create new files

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-import App from './App.tsx'
+import App from './App.js'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
 )
